52dfbce495f7b3058856537752eadbf6d1bc5466
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-11 12:34:05 +0900

    feat: greet()

--- src/JlTrussC.cpp
+++ src/JlTrussC.cpp
@@ -1,1 +1,13 @@
 #include "JlTrussC.h"
+
+#include "jlcxx/jlcxx.hpp"
+
+std::string greet()
+{
+   return "Hello from JlTrussC C++!!";
+}
+
+JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
+{
+  mod.method("greet", &greet);
+}
