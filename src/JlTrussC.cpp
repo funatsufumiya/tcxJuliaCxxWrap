c1d69804d12379560022a2c71277331e2df81428
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-11 12:26:53 +0900

    feat: runTrusscTestApp()

--- src/JlTrussC.cpp
+++ src/JlTrussC.cpp
@@ -1,1 +1,37 @@
 #include "JlTrussC.h"
+
+#include "jlcxx/jlcxx.hpp"
+
+#include "TrussC.h"
+using namespace std;
+using namespace tc;
+
+class TestApp : public App {
+    void draw() override {
+        clear(0.12f);
+
+        // Rotating box
+        noFill();
+        translate(getWindowWidth() / 2, getWindowHeight() / 2);
+        rotate(getElapsedTimef() *0.1f, getElapsedTimef() *0.15f, 0);
+        drawBox(200.0f);
+    }
+};
+
+void runTrusscTestApp() {
+    WindowSettings settings;
+    settings.setSize(960, 600);
+    settings.setTitle("TrussC");
+    return runApp<TestApp>(settings);
+}
+
+std::string greet()
+{
+   return "Hello from JlTrussC C++!!";
+}
+
+JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
+{
+  mod.method("greet", &greet);
+  mod.method("runTrusscTestApp", &runTrusscTestApp);
+}
