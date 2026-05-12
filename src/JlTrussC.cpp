0493256ef110962650a5b13911eeeb056576b502
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-13 07:02:24 +0900

    feat: runTrusscApp(), keyPressed

--- src/JlTrussC.cpp
+++ src/JlTrussC.cpp
@@ -11,6 +11,8 @@ using namespace tc;
 static void *setupFnPtr = nullptr;
 static void *updateFnPtr = nullptr;
 static void *drawFnPtr = nullptr;
+static void *keyPressedFnPtr = nullptr;
+static void *keyReleasedFnPtr = nullptr;
 
 class TestApp : public App {
     void draw() override {
@@ -24,10 +26,10 @@ class TestApp : public App {
     }
 };
 
-void runTrusscTestApp() {
+void runTrusscTestApp(int width=960, int height=600, const std::string& title="TrussC") {
     WindowSettings settings;
-    settings.setSize(960, 600);
-    settings.setTitle("TrussC");
+    settings.setSize(width, height);
+    settings.setTitle(title);
     runApp<TestApp>(settings);
 }
 
@@ -43,11 +45,27 @@ void setDrawFn(jl_value_t *fn){
     drawFnPtr = jl_unbox_voidpointer(fn);
 }
 
+void setKeyPressedFn(jl_value_t *fn){
+    keyPressedFnPtr = jl_unbox_voidpointer(fn);
+}
+
+void setKeyReleasedFn(jl_value_t *fn){
+    keyReleasedFnPtr = jl_unbox_voidpointer(fn);
+}
+
 void callFnPtr(void* ptr){
     if(ptr != nullptr){
         typedef void (*CppPtr)();
         CppPtr rePtr = reinterpret_cast<CppPtr>(ptr);
         rePtr();
+    }
+}
+
+void callKeyFnPtr(void* ptr, int key){
+    if(ptr != nullptr){
+        typedef void (*CppPtr)(int key);
+        CppPtr rePtr = reinterpret_cast<CppPtr>(ptr);
+        rePtr(key);
     }
 }
 
@@ -63,6 +81,59 @@ void callDrawFn(){
     callFnPtr(drawFnPtr);
 }
 
+void callKeyPressedFn(int key){
+    callKeyFnPtr(keyPressedFnPtr, key);
+}
+
+void callKeyReleasedFn(int key){
+    callKeyFnPtr(keyReleasedFnPtr, key);
+}
+
+class MyApp : public App {
+    void setup() override {
+        callSetupFn();
+    }
+
+    void update() override {
+        callUpdateFn();
+    }
+
+    void draw() override {
+        callDrawFn();
+    }
+
+    void keyPressed(int key) override {
+        callKeyPressedFn(key);
+    }
+
+    void keyReleased(int key) override {
+        callKeyReleasedFn(key);
+    }
+
+    void mousePressed(Vec2 pos, int button) override {
+
+    }
+    void mouseReleased(Vec2 pos, int button) override {
+
+    }
+    void mouseMoved(Vec2 pos) override {
+
+    }
+    void mouseDragged(Vec2 pos, int button) override {
+
+    }
+    void mouseScrolled(Vec2 delta) override {
+
+    }
+};
+
+void runTrusscApp(int width=960, int height=600, const std::string& title="TrussC") {
+    WindowSettings settings;
+    settings.setSize(width, height);
+    settings.setTitle(title);
+    runApp<MyApp>(settings);
+}
+
 std::string greet()
 {
    return "Hello from JlTrussC C++!!";
@@ -72,10 +143,19 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
   mod.method("greet", &greet);
   mod.method("runTrusscTestApp", &runTrusscTestApp);
+  mod.method("runTrusscTestApp", [](){ runTrusscTestApp(); });
+  mod.method("runTrusscTestApp", [](int w, int h){ runTrusscTestApp(w, h); });
+  mod.method("runTrusscApp", &runTrusscApp);
+  mod.method("runTrusscApp", [](){ runTrusscApp(); });
+  mod.method("runTrusscApp", [](int w, int h){ runTrusscApp(w, h); });
   mod.method("setSetupFn", &setSetupFn);
   mod.method("callSetupFn", &callSetupFn);
   mod.method("setUpdateFn", &setUpdateFn);
   mod.method("callUpdateFn", &callUpdateFn);
   mod.method("setDrawFn", &setDrawFn);
   mod.method("callDrawFn", &callDrawFn);
+  mod.method("setKeyPressedFn", &setKeyPressedFn);
+  mod.method("callKeyPressedFn", &callKeyPressedFn);
+  mod.method("setKeyReleasedFn", &setKeyReleasedFn);
+  mod.method("callKeyReleasedFn", &callKeyReleasedFn);
 }
